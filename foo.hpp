46ba01191f60425ff5834e8764c53958874b9503
Benjamin Kircher <benjamin.kircher@gmail.com>
2017-10-19 16:28:33 +0200

    Ehm, fix example
    Behavior of toupper is undefined if the argument's value is neither
    representable as unsigned char nor equal to EOF. The argument should
    therefore first be converted to an unsigned char.

--- foo.hpp
+++ foo.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <algorithm>
+#include <cctype>
 #include <string>
 
 namespace foo {
 inline int multiply(int a, int b) { return a * b; }
 
-inline std::string uppercase(std::string str) {
-  std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-  return str;
+inline std::string uppercase(std::string s) {
+  std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+  return s;
 }
 }
